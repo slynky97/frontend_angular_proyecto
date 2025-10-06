09a24833987fd63b02dacf23a0859f74b5f5ac40
juan marcelo catari ticona <50919460+slynky97@users.noreply.github.com>
2025-10-06 16:21:47 -0400

    login Funcion redirect

--- src/app/auth/components/login/login.ts
+++ src/app/auth/components/login/login.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,21 @@ export class Login {
 
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(8)])
+    password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(20)])
   });
+
+  router = inject(Router);
+
+  funIngresar() {
+    if(this.loginForm.value.email == "admin@mail.com" && this.loginForm.value.password == "admin54321"){
+      this.router.navigate(["/admin/perfil"])
+    }else{
+      alert('Credenciales incorrectas');
+    }
+  }
+
+  funPersonalizado(){
+    alert('Función personalizada ejecutada.');
+  }
 
 }
