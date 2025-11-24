957eddcaebb2bf0b0a65ccccec39c93045f85d9a
juan marcelo catari ticona <50919460+slynky97@users.noreply.github.com>
2025-11-24 11:34:24 -0400

    router link elimado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { AuthRoutingModule } from "./auth/auth-routing-module";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, AuthRoutingModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/web/inicio/inicio.ts
+++ src/app/web/inicio/inicio.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { AuthRoutingModule } from "../../auth/auth-routing-module";
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
-  imports: [RouterLink],
   templateUrl: './inicio.html',
   styleUrl: './inicio.scss'
 })
