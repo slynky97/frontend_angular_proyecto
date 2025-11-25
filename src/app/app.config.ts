d3057b17a12fcdcc75052406d0450f5649ad3f2b
juan marcelo catari ticona <50919460+slynky97@users.noreply.github.com>
2025-11-24 22:21:50 -0400

    emplate dashboard admin reduciendo la version v20 de angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,25 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { authInterceptorInterceptor } from './core/interceptors/auth-interceptor-interceptor';
+import { providePrimeNG } from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes),
+    provideRouter(routes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }), withEnabledBlockingInitialNavigation()),
+    provideAnimationsAsync(),
+    providePrimeNG({ theme: { preset: 'Aura', options: { darkModeSelector: '.app-dark' } } }),
     provideHttpClient(withInterceptors([authInterceptorInterceptor]),withFetch())
   ]
 };
 
+
+function provideAnimationsAsync(): import("@angular/core").Provider | import("@angular/core").EnvironmentProviders {
+  throw new Error('Function not implemented.');
+}
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { Nosotros } from './web/nosotros/nosotros';
 import { Perfil } from './admin/perfil/perfil';
 import { Usuario } from './admin/usuario/usuario';
 import { SitioLayout } from './layout/sitio-layout/sitio-layout';
+import { AppLayout } from './layout/component/app.layout';
 
 export const routes: Routes = [
     {
@@ -27,12 +28,19 @@ export const routes: Routes = [
         ]
     },
     {
-        path: 'admin/perfil',
-        component: Perfil
-    },
-    {
-        path: 'admin/usuario',
-        component: Usuario
-    }
+        path: 'admin',
+        component: AppLayout,
+        children: [
+            {
+                path: 'perfil',
+                component: Perfil
+            },
+            {
+                path: 'usuario',
+                component: Usuario
+            }
 
+        ]
+    },
+    
 ];
